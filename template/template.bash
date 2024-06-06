90eb7f6a7c1e7d4f36571bf4deeb8f0bd84243a0
Maxime Pomier <maxime@pomier.frr>
2024-06-06 22:04:47 +0200

    init template bash script

---
+++ template/template.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function to display help
+display_help() {
+    echo "Usage: $0 [option...] " >&2
+    echo
+    echo "   -p, --project    Set the project name"
+    echo "   -h, --help       Display help"
+    echo
+    exit 1
+}
+
+while getopts ":p:" opt; do
+    case $opt in
+        p)
+            params_name=$OPTARG
+            echo "Params name: $params_name"
+            # Add your logic here for handling the project name
+            ;;
+        h|\?)
+            display_help
+            ;;
+    esac
+done
